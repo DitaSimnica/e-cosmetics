d825028474b3e7c8f5ec0828c82bf72d37e11b38
DitaSimnica <ds48457@ubt-uni.net>
2024-12-30 15:28:07 +0100

    Added initial model classes

---
+++ backend/Models/CosmeticProducts.cs
@@ -0,0 +1,14 @@
+﻿namespace backend.Models
+{
+    public class CosmeticProducts
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+        public int Quantity { get; set; }
+        public string ImageUrl { get; set; }
+        public int Status { get; set; }
+    }
+}
