6ebad6edc0709d6343d88293282a6bbca2ede9cc
DitaSimnica <ds48457@ubt-uni.net>
2025-01-04 15:56:28 +0100

    Removed .vs directory from Git tracking and updated .gitignore

---
+++ backend/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public UsersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("registration")]
+        public Response register(Users users)
+        {
+            Response response = new Response();
+            DAL dal = new DAL();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("ECosmetics").ToString());
+            response = dal.register(users, connection);
+            return response;
+        }
+    }
+}
