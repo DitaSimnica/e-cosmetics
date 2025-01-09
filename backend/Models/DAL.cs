b0932fc095152796a8d44abf5eccc0a788915aba
ErtaKryeziu <ek67377@ubt-uni.net>
2025-01-09 17:57:00 +0100

    Implemented View/User Functionality

--- backend/Models/DAL.cs
+++ backend/Models/DAL.cs
@@ -43,17 +43,57 @@ namespace backend.Models
             DataTable dt = new DataTable();
             da.Fill(dt);
             Response response = new Response();
+            Users user = new Users();
             if (dt.Rows.Count > 0)
             {
+                user.ID = Convert.ToInt32(dt.Rows[0]["ID"]);
+                user.FirstName = Convert.ToString(dt.Rows[0]["FirstName"]);
+                user.LastName = Convert.ToString(dt.Rows[0]["LastName"]);
+                user.Email = Convert.ToString(dt.Rows[0]["Email"]);
+                user.Type = Convert.ToString(dt.Rows[0]["Type"]);
                 response.StatusCode = 200;
                 response.StatusMessage = "User is valid";
+                response.user = user;
             }
             else
             {
                 response.StatusCode = 100;
                 response.StatusMessage = "User is invalid";
+                response.user = null;
             }
             return response;
         }
+
+        public Response viewUser(Users users, SqlConnection connection)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("p_viewUser", connection);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            da.SelectCommand.Parameters.AddWithValue("@ID", users.ID);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            Response response = new Response();
+            Users user = new Users();
+            if (dt.Rows.Count > 0)
+            {
+                user.ID = Convert.ToInt32(dt.Rows[0]["ID"]);
+                user.FirstName = Convert.ToString(dt.Rows[0]["FirstName"]);
+                user.LastName = Convert.ToString(dt.Rows[0]["LastName"]);
+                user.Email = Convert.ToString(dt.Rows[0]["Email"]);
+                user.Type = Convert.ToString(dt.Rows[0]["Type"]);
+                user.Fund = Convert.ToDecimal(dt.Rows[0]["Fund"]);
+                user.CreatedOn = Convert.ToDateTime(dt.Rows[0]["CreatedOn"]);
+                user.Password = Convert.ToString(dt.Rows[0]["Password"]);
+                response.StatusCode = 200;
+                response.StatusMessage = "User exists.";
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "User does not exist.";
+                response.user = user;
+            }
+            return response;
+        
+    }
     }
 }
