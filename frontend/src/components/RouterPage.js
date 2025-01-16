3e0ecb266e8efcdd13cf12ffba549f145cd80678
leonamaliqii <lm67710@ubt-uni.net>
2025-01-16 18:14:18 +0100

    Added routes to router page

--- frontend/src/components/RouterPage.js
+++ frontend/src/components/RouterPage.js
@@ -1,7 +1,41 @@
 import React from "react";
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Registration from './Registration';
+import Dashboard from './users/Dashboard';
+import Orders from './users/Orders';
+import Profile from './users/Profile';
+import Cart from './users/Cart';
+import CosmeticProductsDisplay from './users/CosmeticProductsDisplay';
+import AdminDashboard from './admin/AdminDashboard';
+import AdminOrders from './admin/AdminOrders';
+import CustomerList from './admin/CustomerList';
+import CosmeticProducts from './admin/CosmeticProducts';
+
+
+
+
 
 export default function RouterPage(){
     return(
-        <div>Router</div>
+        <Router>
+            <Routes>
+              <Route path='/' element={<Login/>}/>
+              <Route path='/registration' element={<Registration/>}/>
+              <Route path='/dashboard' element={<Dashboard/>}/>
+              <Route path='/orders' element={<Orders/>}/>
+              <Route path='/profile' element={<Profile/>}/>
+              <Route path='/cart' element={<Cart/>}/>
+              <Route path='/cosmeticproductsdisplay' element={<CosmeticProductsDisplay/>}/>
+
+
+
+              <Route path='/admindashboard' element={<AdminDashboard/>}/>
+              <Route path='/adminorders' element={<AdminOrders/>}/>
+              <Route path='/customerlist' element={<CustomerList/>}/>
+              <Route path='/cosmeticproducts' element={<CosmeticProducts/>}/>
+
+                </Routes>
+        </Router>
     )
 }
