052c293fd24fe119fcdadbf68805a3debfcc5052
DitaSimnica <ds48457@ubt-uni.net>
2025-01-16 17:21:09 +0100

    Created modules and components in React

---
+++ frontend/src/components/admin/CosmeticProducts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function CosmeticProducts(){
+    return(
+        <div>CosmeticProducts</div>
+    )
+}
---
+++ frontend/src/components/users/CosmeticProductsDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function CosmeticProductsDisplay(){
+    return(
+        <div>CosmeticProductsDisplay</div>
+    )
+}
